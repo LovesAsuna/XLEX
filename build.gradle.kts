7798d88f7ec1b7757e012fbb8aee5fc6bec1b489
LovesAsuna <qq625924077@gmail.com>
2021-10-14 19:34:18 +0800

    :tada: Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.30"
+    id("org.jetbrains.compose") version "1.0.0-alpha4-build366"
+}
+
+group = "com.hyosakura"
+version = "1.0"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+dependencies {
+    implementation(project("RegexParser"))
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.5.31")
+    implementation(compose.desktop.currentOs)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.desktop {
+    application {
+        mainClass = "com.hyosakura.xlex.MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Exe)
+            packageName = "XLEX"
+            packageVersion = "1.0.0"
+        }
+    }
+}
